239023dd85df8f3a2c37da562dd0fa86f5912eb0
fedemartino <fedemartino@gmail.com>
2020-03-31 18:30:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conflictos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
