a35bf547dca0799d2a3b49c8cb96bc7671913c38
Nicolás Alliaume <nicolasalliaume@gmail.com>
2020-03-31 18:42:37 -0300

    Cambie el nombre que se imprime

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace conflictos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Soy Nico");
         }
     }
 }
